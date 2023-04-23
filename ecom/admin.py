af6895066e3a75ddc1f98e0735bb08c1a8d80d57
MurotovichSh <124291194+MurotovichSh@users.noreply.github.com>
2023-04-23 22:58:02 +0900

    Add files via upload

---
+++ ecom/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Customer,Product,Order,Feedback
+# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Customer, CustomerAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Order, OrderAdmin)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Feedback, FeedbackAdmin)
+# Register your models here.
