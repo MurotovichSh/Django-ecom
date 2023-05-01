52e2d2c77c7f62cfb73b164605cf311af77e922b
MurotovichSh <124291194+MurotovichSh@users.noreply.github.com>
2023-05-02 03:01:58 +0900

    Update models.py

--- ecom/models.py
+++ ecom/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='profile_pic/CustomerProfilePic/',null=True,blank=True)
     email = models.CharField(max_length=40,null=False,blank=False, unique=False)
     mobile = models.CharField(max_length=20,null=False)
     
